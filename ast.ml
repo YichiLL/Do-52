1452f8845b1a21e253ca2917338665cf54e5fa43
Sinclair Target <sinclairtarget@dyn-160-39-202-112.dyn.columbia.edu>
2014-10-13 18:09:51 -0400

    Added expressions to ast.ml

--- ast.ml
+++ ast.ml
@@ -1,5 +1,15 @@
-type binop = Add | Minus | Multiply | Divide | Equal | Notequal | Lt | Rt | Ltoe
-            | Gtoe | Or | And | Not
+type op = Add | Minus | Multiply | Divide | Equal | Notequal | Lt | Rt | Ltoe
+            | Gtoe | Or | And
+
+type un_op = Not
 
 type expr =
+      Number of int
+    | String of string
+    | Boolean of bool
+    | Id of string
+    | Unop of un_op * expr
+    | Binop of expr * op * expr
+    | Assign of string * expr
+    | Call of string * expr list
 
