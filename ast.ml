96828a716efd60ff66e0fb4fc1f938c2027c5e7b
Sinclair Target <sinclairtarget@dyn-160-39-202-112.dyn.columbia.edu>
2014-10-13 17:59:27 -0400

    Changed op to binop in ast.ml

--- ast.ml
+++ ast.ml
@@ -1,4 +1,4 @@
-type op = Add | Minus | Multiply | Divide | Equal | Notequal | Lt | Rt | Ltoe
+type binop = Add | Minus | Multiply | Divide | Equal | Notequal | Lt | Rt | Ltoe
             | Gtoe | Or | And | Not
 
 type expr =
