35833e986cdbca6486449e0fe2c0e246b9baf86d
Sinclair Target <sinclairtarget@dyn-160-39-148-201.dyn.columbia.edu>
2014-11-12 19:32:30 -0500

    Worked on ast

--- ast.ml
+++ ast.ml
@@ -1,39 +1,47 @@
 type op = Add | Minus | Multiply | Divide | Equal | Notequal | Lt | Gt | Ltoe
-            | Gtoe | Or | And
-
-type un_op = Not
+            | Gtoe | Or | And | Unop
 
 type expr =
       Number of int
     | String of string
     | Boolean of bool
     | Id of string
-    | Unop of un_op * expr
+    | Unop of op * expr
     | Binop of expr * op * expr
-    | Assign of string * expr
-    | Call of string * expr list
 
 type stmt =
       Block of stmt list
-    | Expr of expr
-    | If of expr * stmt * stmt
+    | If of expr * stmt
+    | IfElse of expr * stmt * stmt
     | For of expr * expr * expr * stmt
     | While of expr * stmt
     | Until of stmt * expr
     | SimpLoop of stmt * expr
     | Control of string                 (* e.g. break, continue *)
+    | FuncCall of func_call
+    | Call of string * expr list
+    | Assign of string * expr
+
+(* Record for a function call *)
+type func_call = { 
+    fname : string;
+    formals : expr list;
+}
+
+(* Record for a function declaration *)
+type func_decl = {
+    fname : string; (* Name of the function *)
+    formals : (string * string) list; (* Arguments to the function *)
+    (* locals : string list; WE WILL ADD THIS LATER, IT'S COMPLICATED *)
+    body : stmt list;
+}
+
+(* Missing at the moment: configure, variable decl, type adding *)
+type program = func_decl list
+
+(* STUFF TO ADD LATER:
     | Decl of string * string * expr
     | HasCalled of string * string * string   (* Type has Type called Id *)
     | Config of string * expr
 
-type func_call = { 
-    fname : string;
-    formals : string list;
-}
-
-type func_decl = {
-    fname : string;
-    formals : string list;
-    locals : string list;
-    body : stmt list;
-}
+*)
