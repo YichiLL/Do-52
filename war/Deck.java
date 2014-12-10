a8da27332f3e5550b7b7904aa47c61b95d8c2b65
Josephine Tirtanata <jt2706@columbia.edu>
2014-12-10 14:36:39 -0500

    created a class for Card and Deck. In main, i created a deck of cards, with 3 as the maximum number in descending order. If you run main, it will print out all of the cards

---
+++ war/Deck.java
@@ -0,0 +1,47 @@
+package war;
+
+public class Deck {
+	static final int ASCENDING = 0;
+	static final int DESCENDING = 1;
+	private Card[] deck;
+	public Deck(int maxCard, int order){
+		int minCard;
+		if (order == ASCENDING){
+			minCard = (maxCard + 1) % 13;
+			
+		}else{
+			minCard = maxCard - 1;
+			if(minCard<0)
+				minCard+= 13;
+		}
+
+		deck = new Card[52];
+		for(int i = 0; i<52; i++){
+			
+			int num = i/4;
+			int value;
+			
+			if(order == ASCENDING){
+				value = num - minCard;
+			}
+			else{
+				value = minCard - num;
+			}
+			int suitNum = i%4;
+			int suitVal = 0;
+			if (value<0)
+				value+=13;
+			deck[i] = new Card(num, value, suitNum, suitVal);
+		}
+	}
+	
+	public String toString(){
+		String s = "";
+		for (Card card: deck){
+			s += card.toString() + "\n";
+		}
+		return s;
+	}
+}
+
+
---
+++ war/main.java
@@ -0,0 +1,13 @@
+package war;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//checking for deck
+		Deck deck = new Deck(2, Deck.DESCENDING);
+		System.out.println(deck);
+		
+	}
+
+}
