dd916f11d94036873c1c7a7e63773c9f0fc60a1e
shuuri <jaysonzeon@gmail.com>
2014-12-10 19:10:41 -0500

    Added shuffle, draw, draw_hand functions

--- war/Deck.java
+++ war/Deck.java
@@ -1,8 +1,12 @@
+import java.util.Random;
+
 
 public class Deck {
 	static final int ASCENDING = 0;
 	static final int DESCENDING = 1;
 	private Card[] deck;
+	private int total_cards = 52; // Cards in deck
+	
 	public Deck(int maxCard, int order){
 		int minCard;
 		if (order == ASCENDING){
@@ -40,6 +44,41 @@ public class Deck {
 			s += card.toString() + "\n";
 		}
 		return s;
+	}
+
+	public void shuffle(){
+		Random rnd = new Random();
+		for (int i = 0; i < 52; i++){
+			swap(deck, i, rnd.nextInt(i));
+		} 
+	}
+	
+	private void swap(Card[] d, int i, int j){
+		Card tmp = d[i];
+        d[i] = d[j];
+        d[j] = tmp;
+	}
+	
+	public Card draw(){
+		Random rnd = new Random();
+		int index = 0;
+		do{
+			index = rnd.nextInt( 52 );
+		}while(deck[index] == null);
+		
+		total_cards--;
+		Card tmp = deck[index];
+		deck[index] = null;
+		return tmp;
+	}
+	
+	public Card[] draw_hand(){
+		int hand_size = 5;
+		Card[] hand = new Card[hand_size];
+		for(int i = 0; i < hand_size; i++ ){
+			hand[i] = draw();
+		}
+		return hand;
 	}
 }
 
