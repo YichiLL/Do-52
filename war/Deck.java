6146b3b2c6b55374702025b18768067f873eddca
Josephine Tirtanata <jt2706@columbia.edu>
2014-12-10 21:08:02 -0500

    I changed deck from an array to an array list because we have to deal with a set of cards changing in length. Also, I made deck an inheritence of set. Set is the general class for a group of cards.

--- war/Deck.java
+++ war/Deck.java
@@ -1,13 +1,11 @@
-import java.util.Random;
+import java.util.ArrayList;
 
-
-public class Deck {
+public class Deck extends Set{
 	static final int ASCENDING = 0;
 	static final int DESCENDING = 1;
-	private Card[] deck;
-	private int total_cards = 52; // Cards in deck
 	
 	public Deck(int maxCard, int order){
+		super(null);
 		int minCard;
 		if (order == ASCENDING){
 			minCard = (maxCard + 1) % 13;
@@ -17,8 +15,7 @@ public class Deck {
 			if(minCard<0)
 				minCard+= 13;
 		}
-
-		deck = new Card[52];
+		ArrayList<Card> deck = new ArrayList<Card>();
 		for(int i = 0; i<52; i++){
 			
 			int num = i/4;
@@ -34,52 +31,17 @@ public class Deck {
 			int suitVal = 0;
 			if (value<0)
 				value+=13;
-			deck[i] = new Card(num, value, suitNum, suitVal);
+			deck.add(new Card(num, value, suitNum, suitVal));
 		}
-	}
-	
-	public String toString(){
-		String s = "";
-		for (Card card: deck){
-			s += card.toString() + "\n";
-		}
-		return s;
-	}
-
-	public void shuffle(){
-		Random rnd = new Random();
-		for (int i = 0; i < 52; i++){
-			swap(deck, i, rnd.nextInt(i));
-		} 
-	}
-	
-	private void swap(Card[] d, int i, int j){
-		Card tmp = d[i];
-        d[i] = d[j];
-        d[j] = tmp;
-	}
-	
-	public Card draw(){
-		Random rnd = new Random();
-		int index = 0;
-		do{
-			index = rnd.nextInt( 52 );
-		}while(deck[index] == null);
+		super.set = deck;
 		
-		total_cards--;
-		Card tmp = deck[index];
-		deck[index] = null;
-		return tmp;
+
 	}
 	
-	public Card[] draw_hand(){
-		int hand_size = 5;
-		Card[] hand = new Card[hand_size];
-		for(int i = 0; i < hand_size; i++ ){
-			hand[i] = draw();
-		}
-		return hand;
-	}
+
+	
+
 }
+
 
 
