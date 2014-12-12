2668cefd0b25f7aeeeb36f97bea7053bb2e95bfd
yunfeiwang <yw2577@columbia.edu>
2014-12-11 21:20:34 -0500

    syntax error

--- semantic.ml
+++ semantic.ml
@@ -10,14 +10,14 @@
 (* env has a field called funcs which is a list of function declarations *)
 (*type env = { 
 mutable funcs: func_decl list; 
-}
+}*)
 
 (* check whether a user assigns a string type to a function name *)
 let check_func_name name = function
 | func -> func.decl_name = name
 
 (* this function checks whether a function's name already exists in env *)
-let func_exist_in_env func env = List.exists (check_func_name func.decl_name)
+(*let func_exist_in_env func env = List.exists (check_func_name func.decl_name)
 env.funcs *)
 
 type symbol_table = {
