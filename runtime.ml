1e7c3d5d957dd127ef846da439e27e996a09cdd5
yunfeiwang <yw2577@columbia.edu>
2014-12-13 15:28:32 -0500

    added func_decl side of the tuples

--- runtime.ml
+++ runtime.ml
@@ -33,7 +33,13 @@ let funcs =
           [{ formal_id = "number";
              formal_type = NumberType; }];
       body = []; },
-      "System.console.println(number)" ) ;  
+      "System.console.println(number)" ) ;
+   ({ decl_name = "output";
+      formals =
+          [{ formal_id = "boolean";
+             formal_type = BooleanType; }];
+      body = []; },
+      "System.console.println(boolean)") ;
    ( {decl_name = "output";
       formals = 
           [ { formal_id = "card";
@@ -46,3 +52,29 @@ let funcs =
              formal_type = PlayerType; }];
       body = []; },
       "System.console.println(player)") ;
+   ({ decl_name = "output";
+      formals =
+          [{ formal_id = "set";
+             formal_type = SetType; }];
+      body = []; },
+      "System.console.println(set)") ;
+   ({ decl_name = "input";
+      formals =
+          [{ formal_id = "str";
+             formal_type = StringType; }];
+      body = []; },
+      "java form of input with new String in")
+   ({ decl_name = "turn";
+      formals =
+          [{ formal_id = "player";
+             formal_type = PlayerType; }];
+      body = []; },
+      "java form of turn with player: player1") ;
+   ({ decl_name = "evaluate";
+      formals = [];
+      body = []; },
+      "java form of evaluate") ; 
+   ({ decl_name = "quit";
+      formals = [];
+      body = []; },
+      "java form of quit") ;
