ab6a50f3ed586b94506c0bd3a70c388763006ebb
yunfeiwang <yw2577@columbia.edu>
2014-12-13 08:51:12 -0500

    added sample tuples to func_decl_to_java

--- runtime.ml
+++ runtime.ml
@@ -5,5 +5,22 @@ That way whenever you need the Java you just look through the list using a call.
 The other list is so that we can type check with Ids that involve dots. It's a list of field decls
 Basically we need to know about the public instance vars in each Java class for each type *)
 
+(* according to sast.ml
+type func_decl = {
+decl_name : string;
+formals : formal list;
+body : stmt list;
+}
+*)
+
 open Sast
 
+type java_call = {
+java_fname : string;
+}
+
+(* a list of tuples: ((funcs:Sast.func_decl),(java:java_call)) *)
+let func_decl_to_java = 
+[ (("Card",[Sast.expr;Sast.expr;Sast.expr;Sast.expr]),"Card");
+  (("toString",[],[]),"toString")
+]
