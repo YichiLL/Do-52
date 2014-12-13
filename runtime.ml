f1f43e58d845d3ffb2e67971e5b9cc11cee234dc
yunfeiwang <yw2577@columbia.edu>
2014-12-13 08:00:57 -0500

    runtime.ml coding tryout

--- runtime.ml
+++ runtime.ml
@@ -1,1 +1,9 @@
-(* this is runtime.ml *)
+(* this is runtime.ml  has a list of (function decl, java form) tuples and a list of field decls for everything "default" we support *)
+
+(* One list is a list of (func_decl, java_form) tuples that are always available in every program. 
+That way whenever you need the Java you just look through the list using a call.fname
+The other list is so that we can type check with Ids that involve dots. It's a list of field decls
+Basically we need to know about the public instance vars in each Java class for each type *)
+
+open Sast
+
