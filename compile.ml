208d5b467049adbb8f99da01a015bd80128bf76c
Sinclair Target <sinclairtarget@gmail.com>
2014-12-14 23:31:49 -0500

    Completed compiler.ml and added hello_world test case.

--- compile.ml
+++ compile.ml
@@ -219,71 +219,15 @@ let java_of_call call =
             end
     | _ -> call.fname ^ "(" ^ java_of_args call.args ^ ")"
         
-(*
-
-let java_of_player p =
-    string_of_int ((int_of_string (String.sub p 6 ((String.length p) - 6 )))-1)
-
-let java_of_objects var =  
-    if Str.string_match (Str.regexp "player[0-9]+")  var 0 then 
-    "players.get(" ^ java_of_player var ^ ")"
-    else
-    match var with
-    | "size" -> "size()"
-    | "top" -> "peek()"
-    | _ -> var
-
-
-
-let java_of_var var =
-    match var with
-    |SimpleId(str) -> if Str.string_match (Str.regexp "player[0-9]+")  str 0 then 
-    "players.get(" ^ java_of_player str ^ ")"
-    else str
-    |DotId(str_list) -> String.concat "." (List.map (fun arg -> java_of_objects arg) str_list)
-
-
-exception UnknownType of string
-
-
-(* ; not appended here, see java_of_stmt *)
-(*let rec output_expr = function
-
-
-    | Number num -> string_of_int num
-    | String str -> "\"" ^ str ^ "\""
-    | Id id -> id
-    | Binop(e1, Add, e2) -> "(" ^ java_of_expr e1 ^ " " ^ java_of_op op ^ " " ^
-                           java_of_expr e2 ^ ")"
-    | _ -> raise (UnknownType ("Argument for output is not valid"))
-*)
-
-
-let rec java_of_output = function
-    | Number num -> "\"" ^ string_of_int num ^"\""
-    | String str -> str
-    | Var var -> java_of_var var
-    | Binop(e1, op, e2) -> if op !=  Add then raise (UnknownType ("Argument for output is not valid"))
-    else " " ^ java_of_output e1 ^ " " ^ java_of_op op ^ " " 
-    ^ java_of_output e2 ^ " "
-    | _ -> raise (UnknownType ("Argument for output is not valid"))
-
-let output_call call=
-    match call.args with
-    | [a]-> "System.out.println(" ^ (java_of_output a) ^ ");"
-    | _ -> raise (UnknownType "Argument for input call not valid.")
-    
-let input_call call=
-    match call.args with
-    | [a]-> (java_of_expr a) ^ " = scanner.nextLine();"
-    | _ -> raise (UnknownType "Argument for input call not valid.")
-
-let normal_call call =
-
+let java_of_update = function
+    | Assign(id, e) -> java_of_var id ^ " = " ^ java_of_expr e ^ ";"
+    | VarDecl(var) -> java_of_type var.var_decl_type ^ " " ^ 
+                      var.var_decl_id ^ " = " ^
+                      java_of_expr var.var_decl_value ^ ";"
 
 let rec java_of_stmt stmt =
     match stmt with
-    | Call call -> java_of_call call
+    | Call(call) -> java_of_call call ^ ";"
     | Update(update) -> java_of_update update
     | If(e, tb, fb) -> "if (" ^ java_of_expr e ^ ")\n" ^ java_of_block tb
                         ^ "else\n" ^ java_of_block fb
@@ -298,16 +242,16 @@ let rec java_of_stmt stmt =
     | Prepend(e1, e2, draw_source) ->
             let source =
                 match draw_source with
-                | Top -> "Set.TOP"
-                | Bottom -> "Set.BOTTOM"
+                | Ast.Top -> "Set.TOP"
+                | Ast.Bottom -> "Set.BOTTOM"
             in
                 "Set.prepend(" ^ java_of_expr e1 ^ ", "  ^ source ^ ", " ^
                 java_of_expr e2 ^ ");\n"
     | Append(e1, e2, draw_source) ->
             let source =
                 match draw_source with
-                | Top -> "Set.TOP"
-                | Bottom -> "Set.BOTTOM"
+                | Ast.Top -> "Set.TOP"
+                | Ast.Bottom -> "Set.BOTTOM"
             in
                 "Set.append(" ^ java_of_expr e1 ^ ", " ^ source ^ ", " ^
                 java_of_expr e2 ^ ");\n"
@@ -331,22 +275,6 @@ let java_of_function func =
         access ^ " void " ^ func.decl_name ^ "(" ^ formals ^ ")\n" ^
         java_of_block func.body 
 
-let interpret_card_name name =
-    match name with
-    | "\"Ace\"" -> 0
-    | "\"Jack\"" -> 10
-    | "\"Queen\""-> 11
-    | "\"King\""-> 12
-    | _ -> raise (UnknownType "Invalid card name")
-
-let interpret_card_number config =
-    match config.config_value with
-    |Number(n) -> config.config_id ^ " = " ^ string_of_int (n-1) ^ ";"
-    |String(str) -> config.config_id ^ " = " ^ string_of_int (interpret_card_name str) ^" ;"
-    |_ -> raise (UnknownType "Invalid type used for configure maxCard statement.")
-(* Semantic check assures us that config_value is either a number or bool *)
-
-
 (* Default config values yet to be implemented *)
 let java_of_game program =
     let config_vars = 
@@ -360,15 +288,13 @@ let java_of_game program =
         "import java.util.ArrayList;\n\n" ^
         "public class Game {\n" ^
         "ArrayList<MyPlayer> players;\n" ^
-        "Scanner scanner;\n" ^
         "Set deck;\n" ^
-        "int playerCount = 4;\n" ^
-        "int maxCard = 12;\n" ^
-        "boolean ascend = true;\n" ^
+        "int numberOfPlayers = 4;\n" ^
+        "int highestCard = 12;\n" ^
+        "boolean ascendingOrder = true;\n" ^
         instance_vars ^ "\n" ^
         "public Game() {\n" ^
         config_vars ^
-        "scanner = new Scanner(System.in);\n" ^
         "deck = new Deck(maxCard, ascend);\n" ^
         "players = new ArrayList<MyPlayer>();\n" ^
         "for(int i = 0; i < playerCount; i++) {\n" ^
@@ -379,8 +305,8 @@ let java_of_game program =
         "}"
 
 let compile lexbuf = 
-    let program = (* This is our AST *)
-        Parser.program Cache.process lexbuf
+    let program = (* This is our SAST. *)
+        Semantic.check_prgm (Parser.program Cache.process lexbuf)
     in let game_file =
         open_out "Game.java"
     in let player_file =
@@ -394,4 +320,3 @@ let _ =
         open_in Sys.argv.(1)
     in
         compile (Lexing.from_channel input_file)
-*)
